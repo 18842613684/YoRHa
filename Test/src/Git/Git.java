38c50572ad06d29b629fea725de5455ea6ec518a
YoRHa2g <69197544+YoRHa2g@users.noreply.github.com>
2020-08-19 16:26:26 +0800

    Update Git.java

--- Test/src/Git/Git.java
+++ Test/src/Git/Git.java
@@ -6,6 +6,7 @@ public class Git {
 		System.out.println("asdasdasd");
 		System.out.println("asdasdasd");
 		System.out.println("asdasdasd");
+		System.out.println("----------------------");
 	}
 
 }
